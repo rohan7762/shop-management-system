bd58ec5cfd3da5510c688b59f4c063a8903780d7
Rohan <rohanshree66@gmail.com>
2025-12-23 06:33:22 +0545

    User authentication with JWT

--- backend/src/config/app.js
+++ backend/src/config/app.js
@@ -2,14 +2,18 @@ const express = require('express');
 const cors = require('cors');
 const errorHandler = require('./middlewares/errorMiddleware');
 
+const authRoutes = require('../routes/authRoute');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=> {
-    res.send("API running");
-})
+app.use('/api/auth', authRoutes);
+
+app.get('/', (req, res) => {
+  res.send('API running');
+});
 
 app.use(errorHandler);
 
--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,24 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-    name: { 
-        type: String,
-        required: true,
-        trim: true
-
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-
-
-},
-{timestamps: true}
+  },
+  { timestamps: true }
 );
+
+module.exports = mongoose.model('User', userSchema);
